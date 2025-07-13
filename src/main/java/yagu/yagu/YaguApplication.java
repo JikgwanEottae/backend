4bcbbebfa951b5014a4e7f781ad40b6a8b3eebc0
Junmo <hongjm1022@gmail.com>
2025-07-13 18:31:11 +0900

    feat: first setting

---
+++ src/main/java/yagu/yagu/YaguApplication.java
@@ -0,0 +1,13 @@
+package yagu.yagu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yagu/yagu/YaguApplicationTests.java
@@ -0,0 +1,13 @@
+package yagu.yagu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
