4079bd2e5b43abca723b1d0141ffad805d55f05a
Junmo <hongjm1022@gmail.com>
2025-07-14 18:03:44 +0900

    feat: Oauth service generate

--- src/main/java/yagu/yagu/security/SecurityConfig.java
+++ src/main/java/yagu/yagu/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import yagu.yagu.service.AuthService;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
