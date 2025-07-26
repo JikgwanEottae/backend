c6f132c196d453998c6b19a459078fb36aa5b50b
Junmo <hongjm1022@gmail.com>
2025-07-26 12:10:47 +0900

    feat: Gamediary entity generate

---
+++ src/main/java/yagu/yagu/diary/repository/GameDiaryRepository.java
@@ -0,0 +1,11 @@
+package yagu.yagu.diary.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import yagu.yagu.diary.entity.GameDiary;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface GameDiaryRepository extends JpaRepository<GameDiary, Long> {
+    List<GameDiary> findAllByUserIdAndGameDateBetween(Long userId, LocalDate start, LocalDate end);
+}
